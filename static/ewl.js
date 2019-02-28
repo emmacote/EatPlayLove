20d63379abe541e106c115bfa209a95212818418
Emma Cote <emmacote950@gmail.com>
2019-02-28 13:43:21 -0500

    Setup of a stub endpoint to populate the food list of the 'eat' dialog.

--- static/ewl.js
+++ static/ewl.js
@@ -2,6 +2,31 @@ $(function(w){
 
 const btnEatClick = (e) => {
     $("#modalEat").modal();
+
+    const req = {
+        url: "/food",
+        method: "get"
+    };
+
+    const promise = $.ajax(req);
+
+    const success = function(data) {
+        const foodList = data["foods"];
+        let i;
+        $("#selectFood").empty();
+
+        for(i=0;i<foodList.length;i++){
+            let optionFood = $("<option>" + foodList[i] + "</option>");
+            optionFood.val(foodList[i]);
+            $("#selectFood").append(optionFood);
+        }
+    };
+
+    const failure = function(res) {
+        alert("eat failure");
+    };
+    
+    promise.then(success, failure);
 };
 
 const btnWeighClick = (e) => {
--- view.py
+++ view.py
@@ -1,5 +1,5 @@
 __author__ = 'Emma Cote'
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
@@ -8,6 +8,12 @@ app.debug = True
 @app.route("/")
 def index():
     return render_template("main.html")
+
+
+@app.route("/food", methods=["GET"])
+def food():
+    stub_result = dict(foods="milk oatmeal water pickles".split())
+    return jsonify(stub_result)
 
 
 if __name__ == '__main__':
