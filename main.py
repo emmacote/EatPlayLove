886f2b47e6cb2e580714b81bacc111cb868bf5eb
Emma Cote <emmacote950@gmail.com>
2019-02-28 15:58:25 -0500

    Re-implementation of flask endpoint getting a list of food from the sqlite3 store and showing it to the user.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 __author__ = 'Emma Cote'
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 app.debug = True
@@ -7,6 +7,15 @@ app.debug = True
 @app.route("/")
 def index():
     return render_template("main.html")
+
+
+@app.route("/food", methods=["GET"])
+def food():
+    from model import Session, Food
+    sess = Session()
+    foods = sess.query(Food).all()
+    food_list = [food.name for food in foods]
+    return jsonify(foods=food_list)
 
 
 if __name__ == '__main__':
