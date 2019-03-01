9fdf2dff51691b07079c4d2bec41faf82e0a73ac
Emma Cote <cotejrp@gmail.com>
2019-03-01 13:41:00 -0500

    Refactor. Abortion pointless because route decorator prevents wrong methods. Also there was redundant code for doing lookups of food lists.

--- app.py
+++ app.py
@@ -12,31 +12,25 @@ def index():
 
 @app.route("/food", methods=["GET", "POST"])
 def food():
+
     sess = Session()
-    if request.method == "GET":
-        food_obs = sess.query(Food).all()
-        foods = []
-        for food in food_obs:
-            foods.append(food.name)
+    msg = ""
 
-
-        res = dict(foods=foods)
-        return jsonify(res)
-
-    elif request.method == "POST":
+    if request.method == "POST":
         new_food = request.json["new_food"]
         food = Food(name=new_food)
         sess.add(food)
         sess.commit()
-
-        food_list = [food.name for food in sess.query(Food).all()]
-
         msg = "Added new food: {}".format(new_food)
-        return_data = dict(msg=msg, foods = food_list)
-        return jsonify(return_data)
-
     else:
-        abort(400)
+        # GET method message
+        msg = "Retrieved list of all foods for this user."
+
+
+    food_list = [food.name for food in sess.query(Food).all()]
+    return_data = dict(msg=msg, foods=food_list)
+    return jsonify(return_data)
+
 
 
 if __name__ == '__main__':
