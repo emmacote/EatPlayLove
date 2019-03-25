4fe2f6c3ef2e944d8a3b8c93b6ab3494c14159f4
Emma Cote <emmacote950@gmail.com>
2019-03-25 12:42:53 -0400

    Move model code to the flask view.

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 __author__ = 'Emma Cote'
 from flask import Flask, render_template, jsonify, request, abort
-from model import Food, Session, Serving
+from sqlalchemy import Table, Column, Integer, String, create_engine, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+
+db_url = "sqlite:///datastore.db"
+eng = create_engine(db_url)
+Base = declarative_base(bind=eng)
+
+
+class Food(Base):
+    __tablename__ = "foods"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+
+
+class Serving(Base):
+    __tablename__ = "servings"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(DateTime)
+    food_id = Column(ForeignKey("foods.id"))
+    qty = Column(Integer)
+    food = relationship("Food")
+
+
+Session = sessionmaker(bind=eng)
+Base.metadata.create_all(eng)
+
 
 app = Flask(__name__)
 app.debug = True
